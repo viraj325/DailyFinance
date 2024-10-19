82bece31658d679251fe24c2e4bb9ad119d17326
Viraj Patel <virajpatel325@gmail.com>
2024-10-19 01:39:33 -0400

    Updating the codebase

--- app/src/main/java/com/marquees/dailyfinance/MainActivity.kt
+++ app/src/main/java/com/marquees/dailyfinance/MainActivity.kt
@@ -3,23 +3,17 @@ package com.marquees.dailyfinance
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.marquees.dailyfinance.operations.FinancialDataParser
 import com.marquees.dailyfinance.ui.theme.DailyFinanceTheme
 
@@ -43,14 +37,12 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun FinancialUI() {
     val parsedFinancialData = FinancialDataParser()
-        .parseFinancialDataFromFile("FinancialData.json")
+        .parseFinancialDataFromFile(LocalContext.current, "FinancialData.json")
 
     Column {
         HorizontalDivider()
         LazyColumn {
             items(parsedFinancialData.size) { index ->
-                // When an item's [name] updates, the adapter for that item
-                // will recompose. This will not recompose when [header] changes
                 FinancialCardUI(parsedFinancialData[index].name)
             }
         }
--- app/src/main/java/com/marquees/dailyfinance/operations/FinancialDataParser.kt
+++ app/src/main/java/com/marquees/dailyfinance/operations/FinancialDataParser.kt
@@ -1,5 +1,6 @@
 package com.marquees.dailyfinance.operations
 
+import android.content.Context
 import android.util.Log
 import com.google.gson.Gson
 import com.marquees.dailyfinance.model.FinancialModel
@@ -12,9 +13,9 @@ class FinancialDataParser {
         return parsedData
     }
 
-    fun parseFinancialDataFromFile(fileName: String): List<FinancialModel> {
+    fun parseFinancialDataFromFile(context: Context, fileName: String): List<FinancialModel> {
         // parse from FinancialData.json
-        val jsonData = this::class.java.classLoader?.getResource(fileName)?.readText()
+        val jsonData = context.assets.open(fileName).bufferedReader().use { it.readText() }
         val gson = Gson()
         val parsedData = gson.fromJson(jsonData, Array<FinancialModel>::class.java).toList()
         Log.d("FinancialDataParser", "parseFinancialData: $parsedData")
